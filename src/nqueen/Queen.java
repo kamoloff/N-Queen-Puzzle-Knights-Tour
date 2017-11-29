10b4823a7cc00d3ddd4d78ece6ad5f435b1a0313
Ozodbek Kamolov <ozodbekkamolov@gmail.com>
2017-11-29 11:41:46 +0900

    part 1

---
+++ src/nqueen/Queen.java
@@ -0,0 +1,135 @@
+package nqueen;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.Toolkit;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author ozzy
+ */
+public class Queen extends JPanel {
+        
+     private static int dimension, dim;
+     public static char[][] t;
+     
+      public void paint(Graphics g){
+        int d = dimension;
+        Image img1 = Toolkit.getDefaultToolkit().getImage("src/pic_queen.png");
+        g.setColor(new Color(255, 189, 35));
+        g.fillRect(100, 100, dim, dim);
+        if(d == 1)
+             g.drawImage(img1, 100, 100, 50, 50 , null , this);
+        else if(d%2==1){
+            for(int i = 100; i <= dim+100; i+=100){
+                for(int j = 100; j <= dim+100; j+=100){
+                    g.clearRect(i, j, 50, 50);
+                }
+            }
+
+            for(int i = 150; i <= dim+50; i+=100){
+                for(int j = 150; j <= dim+50; j+=100){
+                    g.clearRect(i, j, 50, 50);
+                }
+            }
+        }
+        else{
+            for(int i = 100; i <= dim; i+=100){
+                for(int j = 100; j <= dim; j+=100){
+                    g.clearRect(i, j, 50, 50);
+                }
+            }
+
+            for(int i = 150; i <= dim+50; i+=100){
+                for(int j = 150; j <= dim+50; j+=100){
+                    g.clearRect(i, j, 50, 50);
+                }
+            }
+        }
+        
+        
+        for(int i = 0; i < d; i++)
+            for(int j = 0; j < d; j++)
+                if(t[i][j] == 'Q')
+                g.drawImage(img1, 100 + i*50, 100 + j * 50, 50, 50 , null , this);
+        
+        
+    }
+    public Queen() {
+        initComponents();
+    }
+    
+    public static void main(String[] args){
+   
+    dimension = Integer.parseInt(args[0]);
+    dim = dimension * 50;
+    JFrame frame = new JFrame();
+    frame.setSize(dim+200, dim+200);
+    frame.getContentPane().add(new Queen());
+    frame.setLocationRelativeTo(null);
+    frame.setBackground(Color.LIGHT_GRAY);
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    frame.setVisible(true);
+    t = new char[dimension][dimension];
+        enumerate(dimension);
+    }
+    
+	public static boolean isConsistent(int[] q, int n) {
+        for (int i = 0; i < n; i++) {
+            if (q[i] == q[n])             return false;   
+            if ((q[i] - q[n]) == (n - i)) return false;   
+            if ((q[n] - q[i]) == (n - i)) return false;   
+        }
+        return true;
+    }
+
+   public static void printTable(int[] q) {
+        int n = q.length;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (q[i] == j) t[i][j] = 'Q';
+                else t[i][j] = '*';
+            }
+        }  
+    }
+
+   public static void enumerate(int n) {
+        int[] a = new int[n];
+        enumerate(a, 0);
+    }
+
+    public static void enumerate(int[] q, int k) {
+        int n = q.length;
+        if (k == n) printTable(q);
+        else {
+            for (int i = 0; i < n; i++) {
+                q[k] = i;
+                if (isConsistent(q, k)) enumerate(q, k+1);
+            }
+        }
+    }  
+
+    
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
+        this.setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 705, Short.MAX_VALUE)
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 633, Short.MAX_VALUE)
+        );
+    }// </editor-fold>//GEN-END:initComponents
+
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    // End of variables declaration//GEN-END:variables
+}
